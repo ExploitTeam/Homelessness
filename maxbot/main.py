086754eca2e2c349a7ff9893abb4cfa1a65340a7
Yaroslav <chusovitin.yarik.2007@gmail.com>
2026-09-19 17:11:51 +0300

    .

--- maxbot/main.py
+++ maxbot/main.py
@@ -1,26 +1,66 @@
 import asyncio
-
+from inline_keyboard import *
 from maxbot.maxapi import Bot
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
-async def handler(update):
-    print(f"New update for bot: {update}")
-    if update.get("update_type") == "message_created":
-        message = update["message"]
-        chat_id = message["recipient"]["chat_id"]
-        text = message["body"].get("text", "")
-        print(f"Received message: {text} from {chat_id}")
-
-
 TOKEN = os.getenv("MAX_TOKEN", "")
+bot = Bot(TOKEN)
+start_text = ("Привет{} 👋\n"
+              "Это бот, который поможет вам найти место, где можно согреться или переночевать.\n"
+              "Нажмите кнопку 'Отправить геопозицию', чтобы посмотреть ближайшие к вам ночлежки.")
+
+@bot.message_handler(commands=['start'])
+async def only_start(update):
+    user = update.get('message', {}).get('sender', {})
+    name = user.get('first_name', {})
+    id = user.get('user_id', {})
+    if name:
+        name = f", {name}"
+        text = start_text.format(name)
+    else:
+        text = start_text
+    print(text)
+
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add_button("Отправить геопозицию", button_types.request_geo_location, payload="send_geo")
+    keyboard.add_button("Открыть мини приложение", button_types.callback, payload="open_mini_app")
+    await bot.send_msg(id, text, keyboard.keyboard)
+
+@bot.message_handler(func=lambda msg, tp: tp == "bot_started")
+async def on_start(update):
+    name = update.get('user', {}).get('first_name', None)
+    id = update.get('user', {}).get('user_id', None)
+    if name:
+        name = f", {name}"
+        text = start_text.format(name)
+    else:
+        text = start_text
+
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add_button("Отправить геопозицию", button_types.request_geo_location, payload="send_geo")
+    keyboard.add_button("Открыть мини приложение", button_types.callback, payload="open_mini_app")
+    await bot.send_msg(id, text, keyboard.keyboard)
+    print(text)
+
+@bot.message_handler(func=lambda msg, tp: tp == "bot_stopped")
+async def on_stop(update):
+    print("BOT DELETED! INFO: ", update)
+
+@bot.message_handler(func=lambda msg, tp:
+                            msg.get('callback', {}).get('payload', '') == "send_geo")
+async def on_callback_hello(update):
+    user =  update.get('callback', {}).get('user', {})
+    name = user.get('first_name', "")
+    id = user.get('user_id', 0)
+    if id:
+        await bot.send_msg(id, "Привет - привет")
 
 async def main():
-    bot = Bot(TOKEN)
+
     try:
         bot.connect()
-        await bot.pulling(handler)
+        await bot.pulling()
     finally:
         await bot.close()
 
--- maxbot/maxapi.py
+++ maxbot/maxapi.py
@@ -7,6 +7,8 @@ from exceptions_max import *
 class Bot:
     api_url = "https://platform-api2.max.ru"
     def __init__(self, token):
+        self.text_handlers = []
+        self.command_handlers = {}
         self.token = token
         self.session = None
         self.ssl_context = ssl.create_default_context(cafile="Russian_Trusted_CA.pem")
@@ -17,19 +19,43 @@ class Bot:
                      "Content-Type": "application/json",},
         )
 
-    async def send_msg(self, chat_id : int, text : str):
+    async def send_msg(self, chat_id : int, text : str, btns: list = []):
         """Send a message to chat"""
 
+        body = {'text': text,
+                  'attachments': [
+                      {
+                          "type": "inline_keyboard",
+                          "payload": {
+                              "buttons": btns
+                          }
+                      }
+                  ] if btns else []}
+        print(body)
         async with self.session.post(
             f'{self.api_url}/messages',
-            params={'chat_id': chat_id},
-            json={'text': text},
+            params={'user_id': chat_id},
+            json=body,
             ssl=self.ssl_context,
         ) as response:
             data = await response.json()
             if response.status != 200:
                 raise ConnectionMaxError(f"Error while sending message. Code: {response.status}")
             return data
+
+    def message_handler(self, commands=None, func=None):
+        def decorator(handler_func):
+            if commands:
+                for cmd in commands:
+                    cmd_name = cmd.lstrip('/')
+                    self.command_handlers[cmd_name] = handler_func
+            elif func:
+                self.text_handlers.append((func, handler_func))
+            else:
+                self.text_handlers.append((lambda msg, _: True, handler_func))
+            return handler_func
+
+        return decorator
 
     async def __get_updates(self, timeout=10):
         """GET NEW EVENTS"""
@@ -43,18 +69,41 @@ class Bot:
                 raise ConnectionMaxError(f"Error while getting updates. Code: {response.status}")
             return data
 
-    async def pulling(self, handler):
+    async def _route_update(self, update):
+        tp = update.get("update_type")
+        #print(tp)
+        message = update.get("message", {})
+
+        if tp == "message_created":
+            text = message.get("body", {}).get("text", "").strip()
+            if text and text.startswith('/'):
+                command = text.split()[0][1:]
+                if command in self.command_handlers:
+                    await self.command_handlers[command](update)
+                    return
+
+        for filter_func, handler_func in self.text_handlers:
+            try:
+                if filter_func(update, tp):
+                    await handler_func(update)
+            except Exception as filter_err:
+                print(f"Ошибка в фильтре: {filter_err}")
+                continue
+        print(tp, update)
+
+    async def pulling(self):
         """Cycle for getting events (messages)"""
         print("Bot started.")
         while True:
-            try:
+            #try:
                 updates = await self.__get_updates()
                 for upd in updates.get("updates", []):
-                    await handler(upd)
-            except Exception as e:
-                warnings.warn(f"ERROR WHILE PULLING: {e}")
-                await asyncio.sleep(2)
+                    await self._route_update(upd)
+            #except Exception as e:
+            #    warnings.warn(f"ERROR WHILE PULLING: {e}")
+            #    await asyncio.sleep(2)
 
     async def close(self):
         if self.session:
             await self.session.close()
+
