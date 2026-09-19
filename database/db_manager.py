4683ee0e55e382d35bf261b9e169dddd881a5ed6
JustWain <goshagoog@gmail.com>
2026-09-19 19:39:28 +0500

    db manager draft

---
+++ database/db_manager.py
@@ -0,0 +1,12 @@
+from
+
+def initialize_database():
+    pass
+
+
+def get_user(id : int = 0, username : str = "", id_homestay : int = 0):
+    pass
+
+
+def get_homestay(address: str = "", is_open : bool = False, id : int = 0):
+    pass
