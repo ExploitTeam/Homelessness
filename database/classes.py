7a79fe52ef39fae7796bc455e0d59e514fc57f64
Yaroslav <chusovitin.yarik.2007@gmail.com>
2026-09-22 22:55:47 +0300

    .

--- database/classes.py
+++ database/classes.py
@@ -73,7 +73,7 @@ class Homestay:
         self.additional_info = additional_info
         self.homestay_type = homestay_type
 
-        if longtitude is None and latitude is None:
+        if not longtitude or not latitude:
             self.latitude, self.longtitude, _ = coordinates_finder_v2.get_coordinates(address)
         else:
             self.latitude = latitude
