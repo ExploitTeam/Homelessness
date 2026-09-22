b7ed775b7847484782eb2f340b4ae319e2ff320a
JustWain <goshagoog@gmail.com>
2026-09-22 22:21:58 +0500

    homestay str method fix

--- database/classes.py
+++ database/classes.py
@@ -87,7 +87,7 @@ class Homestay:
         if usr:
             name = usr.username
         return (f"📍 Адрес: {self.address}\n"
-                f"🔵 Тип: {HomestayTypes.homestay_type.label() if self.homestay_type else '-'}\n"
+                f"🔵 Тип: {HomestayTypes.homestay_type.label if self.homestay_type else '-'}\n"
                 f"😎 Менеджер: {name if name else 'n/a'}\n"
                 f"👤 Всего мест: {self.all_beds}\n"
                 f"👤 Свободно: {self.available_beds}\n"
